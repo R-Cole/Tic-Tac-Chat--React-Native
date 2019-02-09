cc2121b68d716066609c723f13700685ed8bee1a
R-Cole <randy@goodglue.com>
2019-02-09 04:12:17 -0700

    game input and render working

--- components/Chat.js
+++ components/Chat.js
@@ -20,85 +20,201 @@ class Chat extends React.Component {
   constructor(props){
     super(props);
 
-    let testX=[];
-    let testY=[];
-    let testType=[];
-    let gamePieces=[];
+    let setTypes=[];
  
     for (let i=1;i<26;i++){
-        gamePieces[i]={type:'UA'};
+      setTypes[i]={ type:'UA' };
     }
 
-    //console.log('gamePieces in state: ',gamePieces);
+    setTypes[0]='INTENTIONALLY NOT USED!';
+
+    console.log('>>>>>>> LOADING types in state >>>>>>>>: ',setTypes);
  
     this.state = {
 
+      gameMode: 'Active',
+      currentTeam: 'X',
       messages: [],
-      gamePieces:gamePieces,
-      XO_view: The_X,
-      stuff:'',
-      inputXY: new Animated.ValueXY({x:500,y:0}) 
+      types: setTypes,
+      stuff:'' 
 
     }
-
-
-    this.moveAnimation = new Animated.ValueXY({ x:500,y:0 })
-  
-
+   
   }
+   
+
+  componentDidMount(){
+
+    Fire.shared.on(message => {
   
+      //kill keyboard once you send text...
+      dismissKeyboard();
+ 
+      console.log('--------------------------------incoming message: ',message);
+    
+      let newArray=this.state.types;
+
+      //if the state has been loaded...
+      //if(this.state){
+
+      let newValue = 0;
+ 
+      for(let i=0;i<message.text.length;i++){
+ 
+        //X move is found
+        if(message.text[i].toLowerCase() === 'x' && message.text[i+1].toLowerCase() === 'x' && message.text[i+2].toLowerCase() === 'x'){
+ 
+          console.log('xxx is found');
+
+          //Get 2 digit number
+          newValue = (parseInt(message.text[i+3] + message.text[i+4]));
+
+          //Check for 1 digit number
+          if(newValue === NaN){
+            newValue = (parseInt(message.text[i+3]));
+          }
+
+          //No good number found exit
+          if(newValue === NaN){
+            return;
+          }
+ 
+          //Value is good but outside of array range
+          if(newValue < 0 || newValue > 25){
+            return;
+          }
+
+          let newType = {
+            type: 'X'
+          }
+
+          newArray = this.state.types.map((item,index) => {
+
+            if(index === newValue){
+
+              return newType;
+
+            }else{
+
+              return item;
+
+            }
+
+          });
+
+        }
+
+        //O move is found
+        if(message.text[i].toLowerCase() === 'o' && message.text[i+1].toLowerCase() === 'o' && message.text[i+2].toLowerCase() === 'o'){
+ 
+          console.log('ooo is found');
+
+          //Get 2 digit number
+          newValue = (parseInt(message.text[i+3] + message.text[i+4]));
+
+          //Check for 1 digit number
+          if(newValue === NaN){
+            newValue = (parseInt(message.text[i+3]));
+          }
+
+          //No good number found exit
+          if(newValue === NaN){
+            return;
+          }
+ 
+          //Value is good but outside of array range
+          if(newValue < 0 || newValue > 25){
+            return;
+          }
+
+          let newType = {
+            type: 'O'
+          }
+
+
+          newArray = this.state.types.map((item,index) => {
+
+            if(index === newValue){
+
+              return newType;
+
+            }else{
+
+              return item;
+
+            }
+
+          });
+
+           
+  
+          //console.log('newArray ',newArray);
+
+           
+        }
+
+        //qqq restart the game
+        if(message.text[i].toLowerCase() === 'q' && message.text[i+1].toLowerCase() === 'q' && message.text[i+2].toLowerCase() === 'q'){
+
+          for (let i=1;i<26;i++){
+            newArray[i]={ type:'UA' };
+          }
+          
+        }
+ 
+      }
+
+      this.setState({
+
+        types: newArray
+
+      })
+
+      console.log('After set state---> ',this.state.types)
+
+
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages,message) 
+        
+      }))
+
+
+      
+      
+    }); 
+
+  };
+
+  componentWillUnmount(){
+    console.log('Unmount has happened!');
+    Fire.shared.off();
+  }
 
   get user(){
-
-    //console.log('Chat.js get user? ',Fire.shared.uid);
-    //console.log('Chat.js messages? ',this.state.messages);
-
+ 
     return{
       name: this.props.navigation.state.params.name,
       _id: Fire.shared.uid,
     };
 
   }
-
-  render(){
-
-    //console.log('Chat.js user?: ',this.user);
-
-    //<View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }} ></View>
-
-    //This will update the view that will render the array of the game pieces positions...
  
-    // for(let i=0;i<this.state.X_positions.length;i++){
- 
-    // }
+render(){
 
-    //use this to setup views each time
-    //const newstuff_1 = [{ transformXY: this.state.gamePieces[1].y }, {transformXY: this.state.gamePieces[1].x}];
-    // const newstuff_2 = [{ translateY: this.state.gamePieces[2].y }, {translateX: this.state.gamePieces[2].x}];
-    // const newstuff_3 = [{ translateY: this.state.gamePieces[3].y }, {translateX: this.state.gamePieces[3].x}];
-    // const newstuff_4 = [{ translateY: this.state.gamePieces[4].y }, {translateX: this.state.gamePieces[4].x}];
-    // const newstuff_5 = [{ translateY: this.state.gamePieces[5].y }, {translateX: this.state.gamePieces[5].x}];
-    // const newstuff_6 = [{ translateY: this.state.gamePieces[6].y }, {translateX: this.state.gamePieces[6].x}];
+  let stopper = 0;
 
-     
-     
+  //console.log('----->>>> AT RENDER: this.state.types ',this.state.types[1].type);
 
-    // let testThing = <Animated.Image source={The_O} style={{transform: newstuff}}/>
- 
-    // let addView = null;
-      
+  if(this.state.gameMode === undefined){stopper = 1;}
 
+  if(this.state.gameMode === 'Active'){stopper = 0;}
 
-     
-    //bigView = <Animated.Image source={this.state.gamePieces[1].type} style={styles.pos_1}/>;
+  //No State Yet 
+  if(stopper === 1) {
 
- //<View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
-//No State Yet 
-if(this.state.gamePieces[1].type === undefined){
-
-  return(
-    <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+    //Render view with no game pieces...
+    return(
+      <View style={{flex: 1, backgroundColor: 'powderblue'}}>
       
       <Image source={GameBoard} style={styles.GameBoardContainer}/>  
  
@@ -109,225 +225,92 @@ if(this.state.gamePieces[1].type === undefined){
       />
       
     </View>
-  );
+    );
 
-   
-}
-
-//State is loaded
-if(this.state.gamePieces[1].type !== undefined){
-
-  let setTypes = [];
+  } 
+  
+  //State is loaded
+  if(stopper === 0) {
  
-  for (let i=1; i < 26; i++){
-   
-        //UA --unassigned
-        if(this.state.gamePieces[i].type === 'UA'){
-          setTypes[i] = Blank;
-        }
-  
-        //X
-        if(this.state.gamePieces[i].type === 'X'){
-          setTypes[i] = The_X;
-        }
-  
-        //Y
-        if(this.state.gamePieces[i].type === 'O'){
-          setTypes[i] = The_O;
-        }
+      let setTypes = [];
+     
+      for (let i=1; i < 26; i++){
        
-  }
-
-  return(
-    <View style={{flex: 1, backgroundColor: 'powderblue'}}>
-      
-        <Image source={GameBoard} style={styles.GameBoardContainer}/>  
-
-        {/* <Animated.Image source={The_X} style={[this.moveAnimation.getLayout()]}/> */}
-        <View style={styles.NewContainer}>
-         <Image source={setTypes[1]} style={styles.pos_1}/>
-         <Image source={setTypes[2]} style={styles.pos_2}/>
-         <Image source={setTypes[3]} style={styles.pos_3}/>
-         <Image source={setTypes[4]} style={styles.pos_4}/>
-         <Image source={setTypes[5]} style={styles.pos_5}/>
-         <Image source={setTypes[6]} style={styles.pos_6}/>
-         <Image source={setTypes[7]} style={styles.pos_7}/>
-         <Image source={setTypes[8]} style={styles.pos_8}/>
-         <Image source={setTypes[9]} style={styles.pos_9}/>
-         <Image source={setTypes[10]} style={styles.pos_10}/>
-         <Image source={setTypes[11]} style={styles.pos_11}/>
-         <Image source={setTypes[12]} style={styles.pos_12}/>
-         <Image source={setTypes[13]} style={styles.pos_13}/>
-         <Image source={setTypes[14]} style={styles.pos_14}/>
-         <Image source={setTypes[15]} style={styles.pos_15}/>
-         <Image source={setTypes[16]} style={styles.pos_16}/>
-         <Image source={setTypes[17]} style={styles.pos_17}/>
-         <Image source={setTypes[18]} style={styles.pos_18}/>
-         <Image source={setTypes[19]} style={styles.pos_19}/>
-         <Image source={setTypes[20]} style={styles.pos_20}/>
-         <Image source={setTypes[21]} style={styles.pos_21}/>
-         <Image source={setTypes[22]} style={styles.pos_22}/>
-         <Image source={setTypes[23]} style={styles.pos_23}/>
-         <Image source={setTypes[24]} style={styles.pos_24}/>
-         <Image source={setTypes[25]} style={styles.pos_25}/>
-
-        </View>
-      
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={Fire.shared.send}
-        user={this.user}
-      />
-      
-    </View>
-  );
- 
-}
-
-
+            if(this.state.types[i].type === undefined){
+              setTypes[i] = Blank;
+            }
     
- 
-  }
-
-  /*
-style={{
-            position: 'absolute',
-            top: 55,
-            bottom: 55,
-            left: 255,
-            right: 255,
-            justifyContent: 'flex-end'}}
-  */
-
-
-  componentDidMount(){
-    Fire.shared.on(message => {
-
-      //SPIN STUFF? ----------------->
-
-      // spinValue = new Animated.Value(0)
-
-      // // First set up animation 
-      // Animated.timing(
-      //     this.spinValue,
-      //   {
-      //     toValue: 1,
-      //     duration: 3000,
-      //     easing: Easing.linear
-      //   }
-      // ).start()
+            //UA --unassigned
+            if(this.state.types[i].type === 'UA'){
+              setTypes[i] = Blank;
+            }
       
-      // // Second interpolate beginning and end values (in this case 0 and 1)
-      // const spin = this.spinValue.interpolate({
-      //   inputRange: [0, 1],
-      //   outputRange: ['0deg', '360deg']
-      // })
-
-      //SPIN STUFF? ----------------->
-
-      //kill keyboard once you send text...
-      dismissKeyboard();
-
-      Animated.spring(this.moveAnimation, {
-        toValue: {x: 0, y: 0},
-      }).start();
-  
-
-      //console.log('--------------------------------incoming message: ',message);
-
-      let newStuff = 'Nothing';
-      let newValue = 0;
-
-      for(let i=0;i<message.text.length;i++){
-
-        if(message.text[i] === 'X'){
-
-          //Need to make a new X piece reference...
-
-          // this.setState({
-
-          //   X_positions: [this.this.stateX_positions ...]
-
-          // })
-
-          newStuff = 'X is found';
-          newValue = (parseInt(message.text[i+1] + message.text[i+2] + message.text[i+3]));
-
-          //console.log('Y? :',this.moveAnimation.y);
-
-          let currentY = this.moveAnimation.y;
-
-          //console.log('currentY? :',currentY);
-
-          //1
-          // let new_X=-93;//COLUMN 1 X
-          // let new_Y=-120;//ROW 1 Y
-
-          //2
-          // let new_X=-44;//COLUMN 2 X // COLUMNS are 49px increments!
-          // let new_Y=-120;//ROW 1 Y // ROWS are 49px increments!
-
-          let new_X=-93;// 
-          let new_Y=-71;//ROW 2 Y
-
- 
-          Animated.spring(this.moveAnimation, {
-            toValue: {x:new_X,y:new_Y},
-          }).start();
-
-        }
-
-        // if(message.text[i] === 'Y'){
-
-        //   newStuff = 'Y is found';
-        //   newValue = (parseInt(message.text[i+1] + message.text[i+2] + message.text[i+3]));
-
-
-        //   let currentX = this.moveAnimation.x; 
- 
-        //   Animated.spring(this.moveAnimation, {
-        //     toValue: {x:(0),y:newValue},
-        //   }).start();
-
-        // }
-
-        // if(message.text[i] === 'R'){
-
-        //   newStuff = 'R is found';
-        //   newValue = parseInt(message.text[i+1]);
- 
-        //   Animated.spring(this.moveAnimation, {
-        //     toValue: {x: 0,y:0},
-        //   }).start();
-
-        // }
- 
+            //X
+            if(this.state.types[i].type === 'X'){
+              setTypes[i] = The_X;
+            }
+      
+            //Y
+            if(this.state.types[i].type === 'O'){
+              setTypes[i] = The_O;
+            }
+           
       }
-
-      this.setState(previousState => ({
-        messages: GiftedChat.append(previousState.messages,message)
-      })
+     
+      //console.log('*** ----------------->current state for types: ',this.state.types);
+     
+      //Render View with game pieces
+      return(
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+          
+            <Image source={GameBoard} style={styles.GameBoardContainer}/>  
+    
+            {/* <Animated.Image source={The_X} style={[this.moveAnimation.getLayout()]}/> */}
+            <View style={styles.NewContainer}>
+             <Image source={setTypes[1]} style={styles.pos_1}/>
+             <Image source={setTypes[2]} style={styles.pos_2}/>
+             <Image source={setTypes[3]} style={styles.pos_3}/>
+             <Image source={setTypes[4]} style={styles.pos_4}/>
+             <Image source={setTypes[5]} style={styles.pos_5}/>
+             <Image source={setTypes[6]} style={styles.pos_6}/>
+             <Image source={setTypes[7]} style={styles.pos_7}/>
+             <Image source={setTypes[8]} style={styles.pos_8}/>
+             <Image source={setTypes[9]} style={styles.pos_9}/>
+             <Image source={setTypes[10]} style={styles.pos_10}/>
+             <Image source={setTypes[11]} style={styles.pos_11}/>
+             <Image source={setTypes[12]} style={styles.pos_12}/>
+             <Image source={setTypes[13]} style={styles.pos_13}/>
+             <Image source={setTypes[14]} style={styles.pos_14}/>
+             <Image source={setTypes[15]} style={styles.pos_15}/>
+             <Image source={setTypes[16]} style={styles.pos_16}/>
+             <Image source={setTypes[17]} style={styles.pos_17}/>
+             <Image source={setTypes[18]} style={styles.pos_18}/>
+             <Image source={setTypes[19]} style={styles.pos_19}/>
+             <Image source={setTypes[20]} style={styles.pos_20}/>
+             <Image source={setTypes[21]} style={styles.pos_21}/>
+             <Image source={setTypes[22]} style={styles.pos_22}/>
+             <Image source={setTypes[23]} style={styles.pos_23}/>
+             <Image source={setTypes[24]} style={styles.pos_24}/>
+             <Image source={setTypes[25]} style={styles.pos_25}/>
+    
+            </View>
+          
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={Fire.shared.send}
+            user={this.user}
+          />
+          
+        </View>
+      );
       
-      )
 
-    }) 
-  };
-
-  componentWillUnmount(){
-    console.log('Unmount has happened!');
-    Fire.shared.off();
   }
-
-}
-
-// const testThis = {
-//   position: 'absolute',
-//   top: 0,
-//   bottom: 0,
-//   left: -64,
-//   right: 0,
-// }
-
+    
+  
+}//render end
+ 
+}//class end
 
 //This sets up the board:
     let testX=[];
@@ -353,7 +336,7 @@ style={{
          
         holder[i] = {position: 'absolute',left:testX[i],top:testY[i]};
 
-        console.log('object #: ',i,' contents: ',holder[i]);
+        //console.log('object #: ',i,' contents: ',holder[i]);
   
     }
 
@@ -368,6 +351,9 @@ const styles = StyleSheet.create({
     top: 0,
     left: -64}, 
   NewContainer: {
+    // position: 'absolute',
+    top: -30,
+    left: 34,
     flex: 1
   },
   pos_1: holder[1],
